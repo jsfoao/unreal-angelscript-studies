0b30909a22651bd7d29edf4835865abb4ebcef2c
João Freire <joao.freire@futuregames.nu>
2022-11-22 12:06:48 +0100

    Initial commit

---
+++ Source/AngelscriptTest/AngelscriptTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AngelscriptTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAngelscriptTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAngelscriptTestGameMode();
+};
+
+
+
